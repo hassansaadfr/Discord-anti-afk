75061b37b484fee58235c8ad0e64d4a6630bbf18
hassansaadfr <saadaoui.hassan@laposte.net>
2021-01-05 16:50:59 +0100

    initial-commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const ioHook = require('iohook')
+const robotJS = require('robotjs')
+
+let lastAction = new Date()
+robotJS.setMouseDelay(1000)
+
+const convertMsToMn = ms => {
+  return ms * 1000 * 60
+}
+
+ioHook.on('mousemove', () => {
+  lastAction = new Date()
+})
+
+ioHook.on('mouseclick', () => {
+  lastAction = new Date()
+})
+
+ioHook.on('keydown', () => {
+  lastAction = new Date()
+})
+
+ioHook.start()
+
+console.log('Ready')
+
+setInterval(() => {
+  if (new Date() - lastAction >= convertMsToMn(5)) {
+    const { x, y } = robotJS.getMousePos()
+    robotJS.moveMouse(x + 100, y + 1)
+    robotJS.moveMouse(x, y)
+  }
+}, 10000)
